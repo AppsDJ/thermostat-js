07a351498051a58ea475169c0e0c94752c56751f
Harry Jaggs <harryjaggs92@gmail.com>
2016-08-03 15:10:30 +0100

    adding a minimum temperature - tests passing

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,6 +8,23 @@ describe('Thermostat', function() {
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('increase in temperature with the up button', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in temperature with the down button', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
 });
